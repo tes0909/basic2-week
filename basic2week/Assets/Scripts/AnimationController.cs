702754a83bb6c8b51a646a71d80adc7c0ab870e7
tes0909 <krosa1213@gmail.com>
2024-10-24 00:00:02 +0900

    [fix] animation

--- basic2week/Assets/Scripts/AnimationController.cs
+++ basic2week/Assets/Scripts/AnimationController.cs
@@ -16,6 +16,8 @@ public class AnimationController : MonoBehaviour
     {
         // animationVariable = 0 IDLE animationVariable 1 = JUMP animationVariable 2 = ATTACK
         // animationVariable가 012면 ++, 2보다크면 0
+
+        //animationVariable = (animationVariable < 2) ? ++animationVariable : animationVariable;
         if (animationVariable < 2)
             animationVariable++;
         else
